b06a6798f861f9faf01d5f5fb770b3bc62763c2f
Angel Gabriel Valdez Ortega <20220908@itla.edu.do>
2023-03-04 02:04:15 -0400

    Se agrego la clase persona para compartir las propiedades basicas.

--- itlaAppSol/itlathApp.Web/Models/StudentModel.cs
+++ itlaAppSol/itlathApp.Web/Models/StudentModel.cs
@@ -5,11 +5,6 @@ namespace itlathApp.Web.Models
     public class StudentModel
     {
         public int Id { get; set; }
-
-        public string FirstName { get; set; }
-        
-        public string LastName { get; set; }
-
         public DateTime EnrollmentDate { get; set; }
 
     }
