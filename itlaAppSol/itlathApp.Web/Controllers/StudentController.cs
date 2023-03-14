bafb2a03291d154c412e1c7a501d94c6a0feed39
Angel Gabriel Valdez Ortega <20220908@itla.edu.do>
2023-03-14 16:49:54 -0400

    Habia un error en StudentController en la linea 28 y ya lo he resuelto.

---
+++ itlaAppSol/itlathApp.Web/Controllers/StudentController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace itlathApp.Web.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            List<Models.StudentModel> students
+                = new List<Models.StudentModel>()
+                {
+                    new Models.StudentModel()
+                    {
+                        Id = 1, EnrollmentDate = System.DateTime.Now, FirstName = "Angel", LastName = "Ortega", Email = "qweqws@gmail.com"
+                    },
+                    new Models.StudentModel()
+                    {
+                        Id = 2, EnrollmentDate = System.DateTime.Now, FirstName = "Carlos", LastName = "Valdez", Email = "uqiyququ@gmail.com"
+                    },
+                    new Models.StudentModel()
+                    {
+                        Id = 3, EnrollmentDate = System.DateTime.Now, FirstName = "Angie", LastName = "Vargas", Email = "nbiahbfd@gmail.com"
+                    }
+                };
+            return View(students);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- itlaAppSol/itlathApp.Web/Models/StudentModel.cs
+++ itlaAppSol/itlathApp.Web/Models/StudentModel.cs
@@ -5,6 +5,7 @@ namespace itlathApp.Web.Models
     public class StudentModel : PersonModel
     {
         public int Id { get; set; }
+
         public DateTime EnrollmentDate { get; set; }
 
     }
